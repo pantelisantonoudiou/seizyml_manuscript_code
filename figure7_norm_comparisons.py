2345edc261ee86cc606b09f35af0e28eefb19b6c
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2025-01-30 14:17:33 -0500

    Create figure7_norm_comparisons.py

---
+++ figure7_norm_comparisons.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+##### ----------------------------- IMPORTS ----------------------------- #####
+import os
+import pandas as pd
+import seaborn as sns
+sns.set(font_scale=1.5)
+sns.set_style("whitegrid")
+##### ------------------------------------------------------------------- #####
+
+metrics = ['percent_detected_seizures',  'f1', 'false_detected_rate']
+hue_order = ['zscore', 'gaussian', 'quantile', 'minmax', ]
+palette = ['#7b3294', '#c2a5cf', '#a6dba0', '#008837']
+# =============================================================================
+#                               Figure 7
+# =============================================================================
+
+# 1 Mouse dataset
+data = pd.read_csv(os.path.join('data', 'trained_models', 'norm_comps', 'mouse', 'test_metrics.csv'))
+melted_data = pd.melt(data, id_vars=['kfold', 'norm_type', 'norm_strategy'], value_vars=metrics)
+g = sns.catplot(data=melted_data, x='norm_strategy', y='value', hue='norm_type', kind='bar',  hue_order=hue_order,
+                height=5, errorbar='se', col='variable',  col_wrap=3, sharey=False, palette=palette)
+
+# 2 MIT dataset - LOO
+data = pd.read_csv(os.path.join('data', 'trained_models', 'norm_comps', 'mit_chb', 'trained_models_loo', 'test_metrics_loo.csv'))
+data['percent_detected_seizures'] = 100*(data['detected_seizures']/data['total_seizures'])
+melted_data = pd.melt(data, id_vars=['subject', 'norm_type', 'norm_strategy'], value_vars=metrics)
+g = sns.catplot(data=melted_data, x='norm_strategy', y='value', hue='norm_type', kind='bar',  hue_order=hue_order,
+                height=5, errorbar='se', col='variable',  col_wrap=3, sharey=False, palette=palette)
+
+# 3 MIT dataset - Inter-Subject
+data = pd.read_csv(os.path.join('data', 'trained_models', 'norm_comps', 'mit_chb', 'trained_models_inter','test_metrics_inter.csv'))
+data['percent_detected_seizures'] = 100*(data['detected_seizures']/data['total_seizures'])
+melted_data = pd.melt(data, id_vars=['group', 'norm_type', 'norm_strategy'], value_vars=metrics)
+g = sns.catplot(data=melted_data, x='norm_strategy', y='value', hue='norm_type', kind='bar',  hue_order=hue_order,
+                height=5, errorbar='se', col='variable',  col_wrap=3, sharey=False, palette=palette)
