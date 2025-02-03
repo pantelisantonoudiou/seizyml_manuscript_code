d46fc8d1d15edea94ed87bb2f332c4e8fe40ad29
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2025-02-03 14:56:09 -0500

    finalize supp fig 5

---
+++ supp_figure_5_gnb_one_feature.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+### -------------- IMPORTS -------------- ###
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+### ------------------------------------- ###
+    
+if __name__ == '__main__':
+    
+    # =============================================================================
+    #                                 Supplementary Figure 5
+    # =============================================================================
+    
+    # settings
+    win = 5
+    time_bounds = 200
+    bins = np.arange(-time_bounds, time_bounds + 1, win)
+    
+    # load data and plot
+    time_data = pd.read_csv(os.path.join('data', 'trained_models', 'per_file', 'trained_models', 'time_predictions_gnb.csv'))
+    score_data = pd.read_csv(os.path.join('data', 'trained_models', 'per_file', 'trained_models', 'gnb_scores_one_feature.csv'))
+    plt.figure()
+    sns.barplot(data=score_data, x='model', y='f1', errorbar='se')
+    plt.figure()
+    sns.histplot(data=time_data, x='time', bins=bins, stat='count', hue='classification', linewidth=1,)
